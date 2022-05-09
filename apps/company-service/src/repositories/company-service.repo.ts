d4cfb7b939d8c8b057e2751af88ea0995f718cb0
Shubham-K-G <sh.shubham.garg@gmail.com>
2022-05-09 11:13:43 +0530

    added try catch

--- apps/company-service/src/repositories/company-service.repo.ts
+++ apps/company-service/src/repositories/company-service.repo.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { FilterQuery, Model } from 'mongoose';
 import { Company, CompanyDocument } from '../schemas/company.schema';
@@ -8,16 +8,34 @@ export class CompanyRepository {
     constructor(@InjectModel(Company.name) private companyModel: Model<CompanyDocument>) {}
 
     async createCompany(company: Company): Promise<Company> {
-        const newCompany = new this.companyModel(company);
-        return await newCompany.save();
+        try{
+            const newCompany = new this.companyModel(company);
+            return await newCompany.save();
+        } catch(error) {
+            console.log(error);
+            throw new BadRequestException("error occured while creating company to db");
+        }
+        
     }
 
     async findOneAndUpdate(companyFilterQuery: FilterQuery<Company> ,company: Partial<Company>): Promise<Company> {
-        return await this.companyModel.findOneAndUpdate(companyFilterQuery, company);
+        try {
+            return await this.companyModel.findOneAndUpdate(companyFilterQuery, company);
+        } catch (error) {
+            console.log(error);
+            throw new BadRequestException("error occured while updating company in db");
+        }
+        
     }
 
     async findAll(companyFilterQuery: FilterQuery<Company>): Promise<Company[]> {
-        if(!companyFilterQuery.companyName || companyFilterQuery.companyName === "") return await this.companyModel.find();
-        else return await this.companyModel.find(companyFilterQuery);
+        try {
+            if(!companyFilterQuery.companyName || companyFilterQuery.companyName === "") return await this.companyModel.find();
+            else return await this.companyModel.find(companyFilterQuery);
+        } catch (error) {
+            console.log(error);
+            throw new BadRequestException("Error occured while finding company/companies in the db");
+        }
+        
     }
 }
