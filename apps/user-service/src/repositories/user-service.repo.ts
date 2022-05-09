b6704a09b9b38f43925a9e210e793d9310cce512
Shubham-K-G <sh.shubham.garg@gmail.com>
2022-05-09 12:50:35 +0530

    added try catch

--- apps/user-service/src/repositories/user-service.repo.ts
+++ apps/user-service/src/repositories/user-service.repo.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { FilterQuery, Model } from 'mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
@@ -8,16 +8,32 @@ export class UserRepository {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
     async createUser(user: User): Promise<User> {
-        const newCompany = new this.userModel(user);
-        return await newCompany.save();
+        try {
+            const newCompany = new this.userModel(user);
+            return await newCompany.save();
+        } catch(error) {
+            console.log(error);
+            throw new BadRequestException("error occured while creating user in db");
+        }
+        
     }
 
     async findOneAndUpdate(userFilterQuery: FilterQuery<User> ,user: Partial<User>): Promise<User> {
-        return await this.userModel.findOneAndUpdate(userFilterQuery, user);
+        try {
+            return await this.userModel.findOneAndUpdate(userFilterQuery, user);
+        } catch (error) {
+            console.log(error);
+            throw new BadRequestException("error occured while updating user in db");
+        }   
     }
 
     async findAll(userFilterQuery: FilterQuery<User>): Promise<User[]> {
-        if(userFilterQuery) return await this.userModel.find(userFilterQuery);
-        else return await this.userModel.find(); 
+        try {
+            if(userFilterQuery) return await this.userModel.find(userFilterQuery);
+            else return await this.userModel.find(); 
+        } catch (error) {
+            console.log(error);
+            throw new BadRequestException("Error occured while finding user/users in the db");
+        }  
     }
 }
